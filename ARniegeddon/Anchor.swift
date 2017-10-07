71f84339289d7076f6126b1f1a5140f623a9c5fa
Xavi Pedrals <xavi.pedrals@gmail.com>
2017-10-07 13:30:48 +1100

    Finished tutorial

---
+++ ARniegeddon/Anchor.swift
@@ -0,0 +1,15 @@
+//
+//  Anchor.swift
+//  ARniegeddon
+//
+//  Created by Xavier Pedrals on 07/10/2017.
+//  Copyright © 2017 Ray Wenderlich. All rights reserved.
+//
+
+import ARKit
+
+class Anchor: ARAnchor {
+  var type: NodeType?
+  
+  
+}
